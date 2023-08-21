3867d7dd8364de4fb2fd0f7063db41ed4baf72e2
jobayerrahman <jobayer.aiub@gmail.com>
2023-08-22 00:52:12 +0600

    Product card design

--- src/Components/Product/Productcard.js
+++ src/Components/Product/Productcard.js
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 export default function Productcard({title,photo,price,des}){
-    const [showMore, setShowMore] = useState(false);
-    const shortendes = des ? des.substring(0, 50) : '';
+    // const [showMore, setShowMore] = useState(false);
+    // const shortendes = des ? des.substring(0, 50) : '';
     return(
         <div className="productCardWrapper">
             <img className='productImage' src={photo}/>
-            <h2>{title}</h2>
-            <h5>{price}</h5>
-            <p>
+            <div className='productCardContent'>
+                <h2>{title}</h2>
+                <h5>{price}</h5>
+            </div>
+            <a>Add to cart</a>
+            {/* <p>
                 {showMore ? des : shortendes}
                 <a className="btn" onClick={()=> setShowMore(!showMore)}>{showMore ? " Show less" : " Show more"}</a>
-            </p>
+            </p> */}
         </div>
     )
 }
