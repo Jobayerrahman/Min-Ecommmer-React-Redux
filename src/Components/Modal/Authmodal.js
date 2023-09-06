accc6ca785edd38f12a5828343cec2e4bf6713c1
jobayerrahman <jobayer.aiub@gmail.com>
2023-09-06 16:18:37 +0600

    Auth and Cart Modal design

--- src/Components/Modal/Authmodal.js
+++ src/Components/Modal/Authmodal.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../../assets/authmodal.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
 
 function Authmodal({showAuth,closeAuth}) {
 
@@ -13,7 +15,7 @@ function Authmodal({showAuth,closeAuth}) {
                         justify-between
                         items-center
                         p-2 m-2'>
-                    <h2 className='text-xl font-bold'>Auth Modal (0)</h2>
+                    <h2 className='text-xl font-bold'>Auth Modal</h2>
                     <FontAwesomeIcon 
                         className='text-dark 
                         mr-2 p-2
@@ -21,6 +23,94 @@ function Authmodal({showAuth,closeAuth}) {
                         hover:cursor-pointer' 
                         onClick={closeAuth}
                         icon={faXmark} />
+                </div>
+                <div className='mx-6 my-10'>
+                    <form>
+                        <label class="block mb-3">
+                            <span class="after:content-['*'] 
+                                    after:ml-0.5 
+                                    after:text-red-500 
+                                    block text-sm 
+                                    font-medium 
+                                    text-slate-700">
+                                Email
+                            </span>
+                            <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="enter your email" />
+                        </label>
+                        <label class="block mb-3">
+                            <span class="after:content-['*'] 
+                                    after:ml-0.5 
+                                    after:text-red-500 
+                                    block text-sm 
+                                    font-medium 
+                                    text-slate-700">
+                                Password
+                            </span>
+                            <input 
+                                type="password" 
+                                name="password" 
+                                class="mt-2 px-3 
+                                    py-2 bg-white 
+                                    border shadow-sm 
+                                    border-slate-300 
+                                    placeholder-slate-400 
+                                    focus:outline-none 
+                                    focus:border-sky-500 
+                                    focus:ring-sky-500 
+                                    block w-full 
+                                    rounded-md sm:text-sm 
+                                    focus:ring-1" 
+                                placeholder="enter your password" />
+                        </label>
+                        <button 
+                            class="bg-slate-900 
+                                py-1.5 px-4 mt-2 
+                                text-center 
+                                text-white 
+                                rounded-md w-full 
+                                font-semibold">
+                            Sign In
+                        </button>
+                    </form>
+                </div>
+                <div className='mx-6 my-4 
+                        p-4 text-center'>
+                    <h6 className='text-sm 
+                            font-normal mb-3
+                            text-slate-400 '>
+                        --------- Don't have account? ---------
+                    </h6>
+                    <button 
+                        class="bg-slate-900 
+                            py-1.5 px-4 mt-2 
+                            text-center 
+                            text-white 
+                            rounded-md w-[140px] 
+                            font-semibold">
+                        Sign Up
+                    </button>
+                    <div className='flex
+                            justify-center mt-6'>
+                        <FontAwesomeIcon 
+                            className='text-dark
+                            text-md 
+                            w-[20px] h-[20px]
+                            mx-2 p-2
+                            text-lg text-white
+                            bg-slate-900
+                            rounded-md
+                            hover:cursor-pointer' 
+                            icon={faGoogle} /> 
+                        <FontAwesomeIcon 
+                            className='text-dark
+                            w-[20px] h-[20px] 
+                            mx-2 p-2
+                            text-lg text-white
+                            bg-slate-900
+                            rounded-md
+                            hover:cursor-pointer' 
+                            icon={faFacebookF} /> 
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/Modal/Cartmodal.js
+++ src/Components/Modal/Cartmodal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../assets/cartmodal.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faL, faXmark } from '@fortawesome/free-solid-svg-icons';
 import productImage from '../../assets/image/image01.jpeg';
 import { useState } from 'react';
 
@@ -10,6 +10,7 @@ function Cartmodal({showCart,closeCart}) {
     const showHiddenCart = showCart ? "modal display-block": "modal display-none";
 
     const [count, setCount] = useState(0);
+    const [isProduct, setIsProduct] = useState(true);
 
     const handleRemoveProduct= () =>{
         if(count !== 0){
@@ -20,6 +21,11 @@ function Cartmodal({showCart,closeCart}) {
     const handleAddProduct= () =>{
         setCount(count+1);
     }
+
+    const handleProductCard= () =>{
+        setIsProduct(false);
+    }
+
     return (
         <div className={showHiddenCart}>
             <div className='modal-main'>
@@ -38,21 +44,81 @@ function Cartmodal({showCart,closeCart}) {
                         icon={faXmark} />
                 </div>
 
-                <div className='flex m-4 w-full'>
-                    <div>
-                        <img src={productImage}/>
-                        <div className='flex flex-col'>
-                            <div className='mx-5'>
-                                <h2 className='text-2xl font-bold mb-2'>Product name</h2>
-                                <spna className='text-xl font-bold mb-2'>$20.00</spna>
+                <div className='flex flex-wrap w-full h-full overflow-y-auto'>
+                    {isProduct? (
+                        <div className='card flex h-[150px]'>
+                            <div className='flex'>
+                                <img className='w-[200px] 
+                                        h-[auto] object-cover
+                                        rounded' 
+                                        src={productImage}/>
+                                <div className='flex flex-col'>
+                                    <div className='mx-5 mb-3'>
+                                        <h2 className='text-2xl 
+                                                font-bold mb-2'>
+                                            Product name
+                                        </h2>
+                                        <spna className='text-sm-[18px] 
+                                                font-medium mb-2'>
+                                            $20.00
+                                        </spna>
+                                    </div>
+                                    <div className='flex flex-nowrap items-center mt-4 mx-2 mb-3'>
+                                        <button 
+                                            className='text-2xl 
+                                                font-extrabold 
+                                                mx-2 px-3 pb-1
+                                                bg-slate-900
+                                                text-white
+                                                rounded-md' 
+                                            onClick={handleRemoveProduct}>
+                                            -
+                                        </button>
+                                        <h6 
+                                            className='text-xl 
+                                                font-bold mx-2'>
+                                                    {count}
+                                        </h6>
+                                        <button 
+                                            className='text-2xl 
+                                                font-extrabold 
+                                                mx-2 px-3 pb-1
+                                                bg-slate-900
+                                                text-white
+                                                rounded-md' 
+                                            onClick={handleAddProduct}>
+                                            +
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='flex flex-nowrap items-center mt-4 mx-2'>
-                                <button className='text-2xl font-bold mx-2' onClick={handleRemoveProduct}>-</button>
-                                <h6 className='text-3xl font-bold mx-2'>{count}</h6>
-                                <button className='text-2xl font-bold mx-2' onClick={handleAddProduct}>+</button>
+                            <div>
+                                <FontAwesomeIcon 
+                                    className='text-dark 
+                                        mr-2 p-2text-lg
+                                        hover:cursor-pointer' 
+                                    onClick={handleProductCard}
+                                    icon={faXmark} />
                             </div>
                         </div>
-                    </div>
+                    ):null}
+                </div>
+                
+                <div className='flex justify-between'>
+                    <h4 className='text-2xl mt-3 
+                            font-bold mb-3
+                            text-slate-400'>
+                        Total - $300.00
+                    </h4>
+                    <button 
+                        class="bg-slate-900 
+                            py-2 px-4 
+                            text-center 
+                            text-white 
+                            rounded-md w-[300px] 
+                            font-semibold">
+                        Checkout 
+                    </button>
                 </div>
             </div>
         </div>
