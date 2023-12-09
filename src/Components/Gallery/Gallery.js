7c378db5f29f71c88bc8e1a1f9558264cbe41617
jobayerrahman <jobayer.aiub@gmail.com>
2023-12-09 23:16:06 +0600

    Gallery component returning to static

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,22 +1,65 @@
 import axios from "axios"; 
 import React, { useEffect, useState } from 'react';
 
-function Gallery() {
-    const [ images, setImages ] = useState([]);
+const initialImageState=[
+    {
+        "id"        : 1,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-1.jpg"
+    },
+    {
+        "id"        : 14,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/pizza/Pizza-3.jpg"
+    },
+    {
+        "id"        : 4,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-4.jpg"
+    },
+    {
+        "id"        : 5,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-5.jpg"
+    },
+    {
+        "id"        : 12,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/pizza/Pizza-1.jpg"
+    },
+    {
+        "id"        : 2,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-2.jpg"
+    },
+    {
+        "id"        : 13,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/pizza/Pizza-2.jpg"
+    },
+    {
+        "id"        : 3,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-3.jpg"
+    },
+    {
+        "id"        : 6,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/burger/Burger-6.jpg"
+    },
+    {
+        "id"        : 15,
+        "image"     : "https://restora-food-ecom.vercel.app/sources/assets/images/pizza/Pizza-4.jpg"
+    }
+]
 
-    useEffect(()=>{
-        axios.get("https://restorajsondata.onrender.com/Restora_Foods_Gallery").then((response) => {
-            const result = response.data;
-            setImages(result);
-            });
-    })
+function Gallery() {
+    const [ images, setImages ] = useState(initialImageState);
+
+    // useEffect(()=>{
+    //     axios.get("https://restorajsondata.onrender.com/Restora_Foods_Gallery").then((response) => {
+    //         const result = response.data;
+    //         setImages(result);
+    //         });
+    // })
     
     return (
         <div>
             <div className='relative flex flex-row flex-wrap mb-4'>
                 <div className='absolute min-h-[100%] w-[100%] bg-[#000] opacity-50'></div>
                 {images.map((img)=>
-                    <img key={img.images} className='w-[150px] h-[200px]
+                    <img key={img.id} className='w-[150px] h-[200px]
                             grow object-cover
                             md:grow-0
                             md:basis-1/2
